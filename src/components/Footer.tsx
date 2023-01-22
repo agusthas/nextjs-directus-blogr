efb9b7966dd9f75d3757da4c305671602716efc7
SusuMantan <suzumantan@gmail.com>
2023-01-22 18:52:57 +0000

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-neutral text-neutral-content">
+      <div>
+        <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 type LayoutProps = {
@@ -6,11 +7,12 @@ type LayoutProps = {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="container mx-auto px-4 py-4 text-primary-content">
+      <main className="flex-1 container mx-auto px-4 py-4 text-primary-content">
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
